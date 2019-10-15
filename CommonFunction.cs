e89d60f785b54c4d34bc61ea3f95129e60fbe3e4
siyecao0314 <55071407+siyecao0314@users.noreply.github.com>
2019-10-15 17:24:47 +0800

    Add files via upload

---
+++ CommonFunction.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace nsStockManage
+{
+    class CommonFunction
+    {
+        //检查编码的合法性函数
+        public static bool checkCodeLegality(String code)
+        {
+            if (code.Length > 10 && code.Contains("-") && code.Length < 30 )
+            {
+                String[] temp = code.Split('-');
+                if (temp.Length == 3)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //检查结尾编码的合法性函数
+        public static bool checkEndCodeLegality(String startCode, String endCode)
+        {
+            if(checkCodeLegality(startCode) && checkCodeLegality(endCode))
+            {
+                String startNumber = startCode.Substring(startCode.Length - 4, 4);
+                String endNumber = endCode.Substring(endCode.Length - 4, 4);
+                if (int.Parse(startNumber) < int.Parse(endNumber))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        //检查字符串中是否包含中文
+        public static bool HasChinese(string str)
+        {
+            return Regex.IsMatch(str, @"[\u4e00-\u9fa5]");
+        }
+    }
+}
